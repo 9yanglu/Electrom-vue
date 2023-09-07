31e90eb11def1188e7cd9870098bb3aca9ce3760
yanglu <yanglu@ifun.com>
2023-09-07 11:21:06 +0800

    feat: 渲染进程集成内置模块

--- src/main/mainEntry.ts
+++ src/main/mainEntry.ts
@@ -1,9 +1,23 @@
 import { app, BrowserWindow } from "electron";
-
+// 设置渲染进程开发者调试工具的警告，这里设置为 true 就不会再显示任何警告了
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
 let mainWindow: BrowserWindow;
 
 app.whenReady().then(() => {
+  let config = {
+    webPreferences: {
+      nodeIntegration: true, // 把Node.js环境集成到渲染进程中
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+      contextIsolation: false, // 在同一个JavaScript上下文中使用Electron API
+      webviewTag: true,
+      spellcheck: false,
+      disableHtmlFullscreenWindowResize: true,
+    },
+  };
   // mainWindow 被设置成一个全局变量，这样可以避免主窗口被 JavaScript 的垃圾回收器回收掉
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow(config);
+  // 打开开发者调试工具
+  mainWindow.webContents.openDevTools({ mode: "undocked" });
   mainWindow.loadURL(process.argv[2]);
 });
