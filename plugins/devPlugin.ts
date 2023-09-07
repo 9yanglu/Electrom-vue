ac853efc709c9ee96684f904405abbf75993c95a
yanglu <yanglu@ifun.com>
2023-09-07 11:31:32 +0800

    feat: 让 Vite 加载 Electron 的内置模块和 Node.js 的内置模块

--- plugins/devPlugin.ts
+++ plugins/devPlugin.ts
@@ -16,18 +16,18 @@ export let devPlugin = () => {
       // 通过监听 server.httpServer 的 listening 事件来判断 httpServer 是否已经成功启动
       server.httpServer?.once("listening", () => {
         let { spawn } = require("child_process");
-        let addressInfo = server.httpServer?.address();
-        let httpAddress = `http://${addressInfo?.address}:${addressInfo?.port}`;
+        let addressInfo = server.httpServer?.address() as any;
+        let httpAddress = `http://${addressInfo.address}:${addressInfo.port}`;
         // 通过 Node.js child_process 模块的 spawn 方法启动 electron 子进程的
         let electronProcess = spawn(
-          require("electron").toString(), //主进程代码编译后的文件路径
-          ["./dist/mainEntry.js", httpAddress], //Vue 页面的 http 地址
+          require("electron").toString(), 
+          ["./dist/mainEntry.js", httpAddress],
           {
             cwd: process.cwd(), // process.cwd() 返回的值就是当前项目的根目录
             stdio: "inherit", // 设置 electron 进程的控制台输出。inherit：以让 electron 子进程的控制台输出数据同步到主进程的控制台（主进程的console.log可以在vscode的控制台上看到）
           }
         );
-        // 当 electron 子进程退出的时候，我们要关闭 Vite 的 http 服务，并且控制父进程退出，准备下一次启动
+        // 当 electron 子进程退出的时候，我们要关闭 Vite 的 http 服务，并且控制父进程退出
         electronProcess.on("close", () => {
           server.close();
           process.exit();
@@ -35,4 +35,41 @@ export let devPlugin = () => {
       });
     },
   };
+};
+
+export let getReplacer = () => {
+  let externalModels = [
+    "os",
+    "fs",
+    "path",
+    "events",
+    "child_process",
+    "crypto",
+    "http",
+    "buffer",
+    "url",
+    "better-sqlite3",
+    "knex",
+  ];
+  let result = {};
+  for (let item of externalModels) {
+    result[item] = () => ({
+      find: new RegExp(`^${item}$`),
+      code: `const ${item} = require('${item}');export { ${item} as default }`,
+    });
+  }
+  result["electron"] = () => {
+    let electronModules = [
+      "clipboard",
+      "ipcRenderer",
+      "nativeImage",
+      "shell",
+      "webFrame",
+    ].join(",");
+    return {
+      find: new RegExp(`^electron$`),
+      code: `const {${electronModules}} = require('electron');export {${electronModules}}`,
+    };
+  };
+  return result;
 };
